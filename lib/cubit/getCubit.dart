2f42cb332aee62ccb23d29c37c3c194dc2935f61
mohamedamr77 <moamr947@gmail.com>
2024-06-23 13:05:02 +0300

    provide cubit

--- lib/cubit/getCubit.dart
+++ lib/cubit/getCubit.dart
@@ -15,5 +15,4 @@ class GetBasketBallCubit extends Cubit<BasketBallState>{
      }
      }
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
+import 'package:basketballsm/cubit/getCubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'features/homeScreen.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return BlocProvider(
+      create: (context) => GetBasketBallCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),),
     );
   }
-
 }
