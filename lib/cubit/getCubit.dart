b354b479192aeee8e5bad95f1cb367a0df1f1aea
mohamedamr77 <moamr947@gmail.com>
2024-06-23 12:36:55 +0300

    add cubit

---
+++ lib/cubit/getCubit.dart
@@ -0,0 +1,7 @@
+import 'package:basketballsm/cubit/getState.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class GetBasketBallCubit extends Cubit<BasketBallState>{
+  GetBasketBallCubit(super.initialState);
+
+}
