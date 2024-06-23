eb7c13a9bac6e631025a7a911fe87cb1b97662cb
mohamedamr77 <moamr947@gmail.com>
2024-06-23 14:43:14 +0300

    finish app

--- lib/features/widgets/score.dart
+++ lib/features/widgets/score.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Score extends StatelessWidget {
-  Score({super.key,required this.score});
-  int score;
+  final int score;
+
+  const Score({Key? key, required this.score}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return  FittedBox(
-      child: Text("$score", style: TextStyle(
-        color: Colors.black,
-        fontSize: 170,
-        fontWeight: FontWeight.w400,
-      ),),
+    double screenWidth = MediaQuery.of(context).size.width;
+    double adaptiveFontSize = screenWidth * 0.21; // Adjust the factor as needed
+
+    return Padding(
+      padding:  EdgeInsets.symmetric(vertical: 40),
+      child: FittedBox(
+        child: SizedBox(
+          width: 130, // Fixed width to ensure space does not change
+          child: Text(
+            "$score",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: adaptiveFontSize,
+              fontWeight: FontWeight.w400,
+            ),
+            textAlign: TextAlign.center, // Center align the text within the fixed width
+          ),
+        ),
+      ),
     );
   }
 }
